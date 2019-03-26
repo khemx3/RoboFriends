1b4aaf27dc5cdb417adbd8baff8ff36855a8f06e
Phakapol Chuliang <phakapol_chuliang@yahoo.com>
2019-03-26 21:51:59 +0700

    filterRobots

--- src/App.js
+++ src/App.js
@@ -13,16 +13,21 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        //this.state.seachfiled  '''BAD!!!!''''
+        this.setState({ searchfield : event.target.value})
+        
     }
 
     render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <CardList robots={filterRobots} />
             </div>
         );
     }
