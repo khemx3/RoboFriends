c71194809a0daa7444c9f06b5812b2db4d9f48bc
Phakapol Chuliang <phakapol_chuliang@yahoo.com>
2019-03-29 18:03:17 +0700

    Scrolling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -35,7 +36,11 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filterRobots} />
+                    <Scroll>
+                        {/* Children  */}
+                        <CardList robots={filterRobots} />
+                    </Scroll>
+                    
                 </div>
             );
         }
